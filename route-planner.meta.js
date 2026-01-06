4d5257751739cb4499278cc1241e8cefd42a1f39
mrboomtastik-coder <mr.boomtastik@gmail.com>
2026-01-06 13:52:02 +0100

    Create route-planner.meta.js

---
+++ route-planner.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @id             iitc-plugin-route-planner
+// @name           IITC Plugin: Portal Route Planner
+// @category       Layer
+// @version        0.1.0
+// @description    Calculate an efficient route through visible portals
+// @updateURL      https://raw.githubusercontent.com/mrboomtastik-coder/iitc-route-planner/main/dist/mrboomtastik-coder/route-planner.meta.js
+// @downloadURL    https://raw.githubusercontent.com/mrboomtastik-coder/iitc-route-planner/main/dist/mrboomtastik-coder/route-planner.user.js
+// @match          https://intel.ingress.com/*
+// @grant          none
+// ==/UserScript==
